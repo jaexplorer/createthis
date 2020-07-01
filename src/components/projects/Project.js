e6eeb05be1c7f02229686baf3761647593fc8b07
andrewsabato <andrew@cognativ.com.au>
2020-07-01 23:49:46 +1000

    size opts

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,20 +1,55 @@
 import React from "react";
 import Close from "../../assets/images/close.png";
-import foodifox from "../../assets/images/page-01.png";
-import movingMindz from "../../assets/images/page-02.png";
-import stemblr from "../../assets/images/page-03.png";
-import pikcha from "../../assets/images/page-04.png";
+// import foodifox from "../../assets/images/page-01.png";
+// import movingMindz from "../../assets/images/page-02.png";
+// import stemblr from "../../assets/images/page-03.png";
+// import pikcha from "../../assets/images/page-04.png";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 const Project = ({ project, changeProject }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      foodifox: file(relativePath: { eq: "page-01.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      movingMindz: file(relativePath: { eq: "page-02.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      stemblr: file(relativePath: { eq: "page-03.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      pikcha: file(relativePath: { eq: "page-04.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `);
+
   const image = () => {
     if (project === "foodifox") {
-      return foodifox;
+      return data.foodifox.childImageSharp.fluid;
     } else if (project === "movingMindz") {
-      return movingMindz;
+      return data.movingMindz.childImageSharp.fluid;
     } else if (project === "stemblr") {
-      return stemblr;
+      return data.stemblr.childImageSharp.fluid;
     } else if (project === "pikcha") {
-      return pikcha;
+      return data.pikcha.childImageSharp.fluid;
     } else return null;
   };
 
@@ -24,7 +59,7 @@ const Project = ({ project, changeProject }) => {
     >
       <img src={Close} onClick={() => changeProject("")} />
       <div className="project-wrapper">
-        <img src={image()} />
+        <Img fluid={image()} />
       </div>
     </div>
   );
